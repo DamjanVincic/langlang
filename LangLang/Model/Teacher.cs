d7dba69af65890e4e1ff237c9b359ce49e9b4060
Mijat Krivokapić <mijat2004@gmail.com>
2024-03-29 17:16:55 +0100

    Change Courses to CourseIds

--- LangLang/Model/Teacher.cs
+++ LangLang/Model/Teacher.cs
@@ -10,14 +10,14 @@ namespace LangLang.Model
     {
         public List<Language> Qualifications {  get; set; }
         public DateOnly DateCreated { get;}
-        public List<Course> Courses { get; set; }
+        public List<int> CourseIds { get; set; }
 
 
-        public Teacher(string firstName, string lastName, string email, string password, Gender gender, string phone, List<Language> qualifications, List<Course> courses) : base(firstName, lastName, email, password, gender, phone)
+        public Teacher(string firstName, string lastName, string email, string password, Gender gender, string phone, List<Language> qualifications, List<int> courseIds) : base(firstName, lastName, email, password, gender, phone)
         {
             Qualifications=qualifications;
-            DateCreated=new DateOnly();
-            Courses=courses;
+            DateCreated=DateOnly.FromDateTime(DateTime.Now);
+            CourseIds=courseIds;
         }
     }
 }
