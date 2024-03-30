6631874ef90aba5fd4530ed23181d220e5e2a25d
Mijat Krivokapić <mijat2004@gmail.com>
2024-03-30 02:05:20 +0100

    Add TeacherViewModel

---
+++ LangLang/ViewModel/TeacherViewModel.cs
@@ -0,0 +1,33 @@
+﻿using LangLang.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.ViewModel
+{
+    public class TeacherViewModel : ViewModelBase
+    {
+        private readonly Teacher teacher;
+
+        public string FirstName=>teacher.FirstName;
+        public string LastName=>teacher.LastName;
+        public string Email => teacher.Email;
+        public string Password => teacher.Password;
+        public Gender Gender => teacher.Gender;
+        public string Phone=>teacher.Phone;
+        public string Qualifications => teacher.Qualifications?.ToString();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public TeacherViewModel(Teacher teacher)
+        {
+            this.teacher=teacher;
+        }
+
+
+    }
+}
