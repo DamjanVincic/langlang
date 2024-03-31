ad0c4e8d8c745e7378bdbe56373c30ae4a130c22
zoricnadja <zoric.n2003@gmail.com>
2024-03-31 23:36:58 +0200

    Add list of languages

--- LangLang/Model/Language.cs
+++ LangLang/Model/Language.cs
@@ -1,15 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LangLang.Model
 {
     public class Language
     {
         private string _name;
+        private static List<Language> _laguages = new List<Language>();
+        private static List<string> _laguageNames = new List<string>();
+
         public Language(string name, LanguageLevel level)
         {
             Name = name;
             Level = level;
+            _laguages.Add(this);
+            if (!_laguageNames.Contains(name))
+            {
+                _laguageNames.Add(name);
+            }
         }
+
         public string Name
         {
             get => _name;
@@ -32,6 +42,12 @@ namespace LangLang.Model
                 throw new InvalidInputException("Name must include at least one character.");
             }
         }
-        
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Language language &&
+                   Name == language.Name &&
+                   Level == language.Level;
+        }
     }
 }
