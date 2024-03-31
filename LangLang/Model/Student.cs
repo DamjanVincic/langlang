75443675e149656005a2e8066754056642e2cf2a
DamjanVincic <damjanvincic2003@gmail.com>
2024-03-31 11:17:42 -0700

    Add Education property to Student model

--- LangLang/Model/Student.cs
+++ LangLang/Model/Student.cs
@@ -2,6 +2,11 @@
 
 public class Student : User
 {
-    public Student(string firstName, string lastName, string email, string password, Gender gender, string phone) :
-        base(firstName, lastName, email, password, gender, phone) { }
+    public Student(string firstName, string lastName, string email, string password, Gender gender, string phone, Education education)
+        : base(firstName, lastName, email, password, gender, phone)
+    {
+        Education = education;
+    }
+
+    public Education Education { get; set; }
 }
