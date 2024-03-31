184dfa6df603820d32483c301eb4f5fb7d779291
zoricnadja <zoric.n2003@gmail.com>
2024-03-31 16:20:48 +0200

    Change ScheduleItem to abstract class

--- LangLang/Model/ScheduleItem.cs
+++ LangLang/Model/ScheduleItem.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LangLang.Model
+﻿namespace LangLang.Model
 {
-    public class ScheduleItem
+    public abstract class ScheduleItem
     {
     }
 }
