9f551e0cac1a07b2745e796877bb9195b65e540f
milicaradicc <milica.t.radic@gmail.com>
2024-03-31 15:29:41 +0200

    Add ExamViewModel

---
+++ LangLang/ViewModel/ExamViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LangLang.Model;
+
+namespace LangLang.ViewModel
+{
+    public class ExamViewModel : ViewModelBase
+    {
+        // glues the model and the view
+        private readonly Exam exam;
+
+        public string Id => exam.Id.ToString();
+        public string Language => exam.Language.Name;
+        public LanguageLevel Level => exam.Language.Level;
+        public string MaxStudents => exam.MaxStudents.ToString();
+        public DateOnly ExamDate => exam.ExamDate;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ExamViewModel(Exam exam)
+        {
+            this.exam = exam;
+        }
+    }
+}
