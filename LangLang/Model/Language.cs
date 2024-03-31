c46c7f34115e3162efe257329f082cc25c031a45
zoricnadja <zoric.n2003@gmail.com>
2024-03-31 15:47:16 +0200

    Refactor code

--- LangLang/Model/Language.cs
+++ LangLang/Model/Language.cs
@@ -1,63 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LangLang.Model
 {
     public class Language
     {
         private string _name;
-        private LanguageLevel _level;
+        public Language(string name, LanguageLevel level)
+        {
+            Name = name;
+            Level = level;
+        }
         public string Name
         {
-            get
-            {
-                return _name;
-            }
+            get => _name;
             set
             {
                 ValidateName(value);
                 _name = value;
             }
         }
-        public LanguageLevel Level
-        {
-            get
-            {
-                return _level;
-            }
-            set
-            {
-                //ValidateLevel(value);
-                _level = value;
-            }
-        }
-
-        //public void ValidateLevel(LanguageLevel level)
-        //{
-        //    if (level == null)
-        //    {
-        //        throw new ArgumentNullException("Level must not be null.");
-        //    }
-        //}
+        public LanguageLevel Level { get; set; }
 
         public void ValidateName(string name)
         {
             if (name == null)
             {
-                throw new ArgumentNullException("Name must not be null.");
+                throw new ArgumentNullException(nameof(_name));
             }
             if (name.Equals(""))
             {
                 throw new InvalidInputException("Name must include at least one character.");
             }
         }
-        public Language(string name, LanguageLevel level) 
-        {
-            Name = name;
-            Level = level;
-        }
+        
     }
 }
