7acafc8c579435b28d76816c3425a2b05748a425
DamjanVincic <damjanvincic2003@gmail.com>
2024-03-31 07:36:08 -0700

    Add Student model

---
+++ LangLang/Model/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace LangLang.Model;
+
+public class Student : User
+{
+    public Student(string firstName, string lastName, string email, string password, Gender gender, string phone) :
+        base(firstName, lastName, email, password, gender, phone) { }
+}
