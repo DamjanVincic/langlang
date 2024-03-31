70ad355bddb383042c268c2d07502c7d24858b44
zoricnadja <zoric.n2003@gmail.com>
2024-03-31 16:15:56 +0200

    Add Schedule to project

---
+++ LangLang/Model/Schedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Model
+{
+    public class Schedule
+    {
+        public Dictionary<DateOnly, ScheduleItem> Table { get; set; }
+    }
+}
