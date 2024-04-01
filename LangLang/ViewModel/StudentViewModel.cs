b0827926403f5ff9d1800e44c154ee8550e93ee2
DamjanVincic <damjanvincic2003@gmail.com>
2024-04-01 14:51:17 -0700

    Add student edit view

--- LangLang/ViewModel/StudentViewModel.cs
+++ LangLang/ViewModel/StudentViewModel.cs
@@ -3,27 +3,35 @@ using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using LangLang.Model;
+using LangLang.View;
 
 namespace LangLang.ViewModel;
 
 public class StudentViewModel : ViewModelBase
 {
-    public Student Student { get; set; }
+    private Student _student;
     public ObservableCollection<Course> AvailableCourses { get; set; }
     public ObservableCollection<Exam> AvailableExams { get; set; }
     
-    public string FullName => $"{Student.FirstName} {Student.LastName}";
+    public string FullName => $"{_student.FirstName} {_student.LastName}";
     
+    public ICommand EditAccountCommand { get;  }
     public ICommand ApplyForCourseCommand { get; }
     public ICommand ApplyForExamCommand { get; }
     
     public StudentViewModel(Student student)
     {
-        Student = student;
+        _student = student;
         // AvailableCourses = new ObservableCollection<Course>(Course.GetAvailableCourses());
         // AvailableExams = new ObservableCollection<Exam>(Exam.GetAvailableExams());
         //
         // ApplyForCourseCommand = new RelayCommand<Course>(ApplyForCourse);
         // ApplyForExamCommand = new RelayCommand<Exam>(ApplyForExam);
+        EditAccountCommand = new RelayCommand(EditAccount);
+    }
+    
+    public void EditAccount()
+    {
+        new StudentEditView(_student).Show();
     }
 }
