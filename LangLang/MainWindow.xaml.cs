74fc011b8b491ff36e52bb7f0e3ff881f8c743bf
DamjanVincic <damjanvincic2003@gmail.com>
2024-04-01 07:05:31 -0700

    Add temporary navigation

--- LangLang/MainWindow.xaml.cs
+++ LangLang/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using LangLang.View;
+using LangLang.ViewModel;
 
 namespace LangLang
 {
@@ -24,16 +25,31 @@ namespace LangLang
         public MainWindow()
         {
             InitializeComponent();
+            // DataContext = new MainViewModel(this.NavigationService);
+            MainFrame.Navigate(new LoginView());
         }
         
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
-        {
-            MainContent.Content = new LoginView();
-        }
+        // private void LoginButton_Click(object sender, RoutedEventArgs e)
+        // {
+        //     // MainContent.Content = new LoginView();
+        // }
+        //
+        // private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        // {
+        //     // MainContent.Content = new RegisterView();
+        // }
 
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
+        
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
-            MainContent.Content = new RegisterView();
+            MainFrame.Navigate(new RegisterView());
         }
     }
 }
