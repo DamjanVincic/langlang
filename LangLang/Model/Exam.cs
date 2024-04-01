57bb71448e29214f183760dcd1d7b13c30b629e2
milicaradicc <milica.t.radic@gmail.com>
2024-04-02 01:59:31 +0200

    Update exam model

--- LangLang/Model/Exam.cs
+++ LangLang/Model/Exam.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 
 namespace LangLang.Model
 {
-    public class Exam
+    public class Exam : ScheduleItem
     {
         private static int _examId = 0;
+        public const int EXAM_DURATION = 360;
         private static Dictionary<int, Exam> _exams = new Dictionary<int, Exam>();
-
         private Language _language;
         private int _maxStudents;
         private DateOnly _examDate;
-        public List<int> StudentIds { get; set; }
 
 
-        public Exam(Language language, int maxStudents, DateOnly examDate)
+        public Exam(Language language, int maxStudents, DateOnly examDate) : base(0, new TimeOnly())
         {
+            throw new NotImplementedException();
             Language = language;
             MaxStudents = maxStudents;
             ExamDate = examDate;
@@ -24,6 +24,8 @@ namespace LangLang.Model
             Id = _examId;
             _exams.Add(Id, this);
         }
+
+        public List<int> StudentIds { get; set; }
 
         public int Id { get; }
 
@@ -80,6 +82,21 @@ namespace LangLang.Model
             {
                 throw new ArgumentNullException("Date of exam must be after today.");
             }
+        }
+        public static Exam GetById(int id)
+        {
+            if (_exams.ContainsKey(id))
+            {
+                return _exams[id];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void Delete(int id)
+        {
+            _exams.Remove(id);
         }
     }
 }
