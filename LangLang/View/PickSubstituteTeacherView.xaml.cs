05ee497a40c0e8af26dac89356c96e737b94a305
Mijat Krivokapić <mijat2004@gmail.com>
2024-04-02 13:32:17 +0200

    Add subtitute teacher picking

---
+++ LangLang/View/PickSubstituteTeacherView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LangLang.Model;
+using LangLang.ViewModel;
+
+namespace LangLang.View
+{
+    /// <summary>
+    /// Interaction logic for PickSubstituteTeacherView.xaml
+    /// </summary>
+    public partial class PickSubstituteTeacherView : Window
+    {
+        public PickSubstituteTeacherView(List<Teacher> availableTeachers, Dictionary<Course, Teacher> substituteTeachers, Course course)
+        {
+            DataContext = new PickSubstituteTeacherViewModel(availableTeachers, substituteTeachers, course);
+            InitializeComponent();
+        }
+    }
+}
