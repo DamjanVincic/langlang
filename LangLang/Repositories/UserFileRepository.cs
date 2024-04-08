36c32d25c1f02f9df5293eebf71e14e9bc7a3e04
DamjanVincic <damjanvincic2003@gmail.com>
2024-04-08 20:15:52 +0200

    Add starting code for user repository

---
+++ LangLang/Repositories/UserFileRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LangLang.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LangLang.Repositories;
+
+public class UserFileRepository : IUserRepository
+{
+    public List<User> GetAll()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public User GetById(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Add(User user)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update(User user)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Delete(User user)
+    {
+        throw new System.NotImplementedException();
+    }
+}
