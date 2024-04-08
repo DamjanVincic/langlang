3acfcbb4797b4b13d4b2cd558c3eeacf98f1102e
DamjanVincic <damjanvincic2003@gmail.com>
2024-04-08 20:43:51 +0200

    Add loading data to user file repository

--- LangLang/Repositories/UserFileRepository.cs
+++ LangLang/Repositories/UserFileRepository.cs
@@ -9,6 +9,17 @@ namespace LangLang.Repositories;
 
 public class UserFileRepository : IUserRepository
 {
+    private const string UserFileName = "users.json";
+    private const string UserDirectoryName = "data";
+
+    private int _idCounter = 1;
+    private Dictionary<int, User> _users = new();
+    
+    public UserFileRepository()
+    {
+        LoadData();
+    }
+    
     public List<User> GetAll()
     {
         throw new System.NotImplementedException();
@@ -32,5 +43,20 @@ public class UserFileRepository : IUserRepository
     public void Delete(User user)
     {
         throw new System.NotImplementedException();
+    }
+    
+    private void LoadData()
+    {
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), UserDirectoryName, UserFileName);
+        
+        if (!File.Exists(filePath)) return;
+
+        string json = File.ReadAllText(filePath);
+        _users = JsonConvert.DeserializeObject<Dictionary<int, User>>(json, new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Auto
+        }) ?? new Dictionary<int, User>();
+            
+        _idCounter = _users.Keys.Max() + 1;
     }
 }
