5ee3db40aca49dfb654668a0ea22dc22f14f5dbc
DamjanVincic <damjanvincic2003@gmail.com>
2024-04-11 01:52:47 +0200

    Add Course service

---
+++ LangLang/Services/CourseService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using LangLang.Model;
+using LangLang.Repositories;
+
+namespace LangLang.Services;
+
+public class CourseService : ICourseService
+{
+    private readonly ICourseRepository _courseRepository = new CourseFileRepository();
+    
+    public List<Course> GetAll()
+    {
+        return _courseRepository.GetAll();
+    }
+
+    public Course? GetById(int id)
+    {
+        return _courseRepository.GetById(id);
+    }
+    
+    public void Add(Language language, int duration, List<Weekday> held, bool isOnline, int maxStudents, int creatorId, TimeOnly scheduledTime, DateOnly startDate, bool areApplicationsClosed, int teacherId, List<int> studentIds)
+    {
+        _courseRepository.Add(new Course(language, duration, held, isOnline, maxStudents, creatorId, scheduledTime, startDate, areApplicationsClosed, teacherId, studentIds));
+    }
+    
+    public void Update(Language language, int duration, List<Weekday> held, bool isOnline, int maxStudents, int creatorId, TimeOnly scheduledTime, DateOnly startDate, bool areApplicationsClosed, int teacherId, List<int> studentIds)
+    {
+        // TODO: Check if teacher is allowed to update the exam
+        // TODO: Decide which information should be updated
+        throw new NotImplementedException();
+    }
+    
+    public void Delete(int id)
+    {
+        _courseRepository.Delete(id);
+    }
+}
---
+++ LangLang/Services/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using LangLang.Model;
+
+namespace LangLang.Services;
+
+public interface ICourseService
+{
+    public List<Course> GetAll();
+    public Course? GetById(int id);
+    public void Add(Language language, int duration, List<Weekday> held, bool isOnline, int maxStudents, int creatorId, TimeOnly scheduledTime, DateOnly startDate, bool areApplicationsClosed, int teacherId, List<int> studentIds);
+    public void Update(Language language, int duration, List<Weekday> held, bool isOnline, int maxStudents, int creatorId, TimeOnly scheduledTime, DateOnly startDate, bool areApplicationsClosed, int teacherId, List<int> studentIds);
+    public void Delete(int id);
+}
