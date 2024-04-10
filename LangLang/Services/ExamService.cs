a5811fbb22bd02901d06b67f28bb51923bb50440
DamjanVincic <damjanvincic2003@gmail.com>
2024-04-11 01:23:52 +0200

    Add exam service

---
+++ LangLang/Services/ExamService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using LangLang.Model;
+using LangLang.Repositories;
+
+namespace LangLang.Services;
+
+public class ExamService : IExamService
+{
+    private readonly IExamFileRepository _examFileRepository = new ExamFileRepository();
+    private readonly IUserRepository _userRepository = new UserFileRepository();
+    
+    public List<Exam> GetAll()
+    {
+        return _examFileRepository.GetAll();
+    }
+
+    public Exam? GetById(int id)
+    {
+        return _examFileRepository.GetById(id);
+    }
+    
+    public void Add(Language language, int maxStudents, DateOnly examDate, int teacherId, TimeOnly examTime)
+    {
+        Teacher teacher = _userRepository.GetById(teacherId) as Teacher ?? throw new InvalidInputException("User doesn't exist.");
+        
+        Exam exam = new Exam(language, maxStudents, examDate, teacherId, examTime);
+        _examFileRepository.Add(exam);
+
+        teacher.ExamIds.Add(exam.Id);
+        _userRepository.Update(teacher);
+    }
+    
+    public void Update(Language language, int maxStudents, DateOnly examDate, int teacherId, TimeOnly examTime)
+    {
+        //TODO: Decide which information should be updated
+        throw new NotImplementedException();
+    }
+    
+    public void Delete(int id)
+    {
+        _examFileRepository.Delete(id);
+    }
+}
---
+++ LangLang/Services/IExamService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using LangLang.Model;
+
+namespace LangLang.Services;
+
+public interface IExamService
+{
+    public List<Exam> GetAll();
+    public Exam? GetById(int id);
+    public void Add(Language language, int maxStudents, DateOnly examDate, int teacherId, TimeOnly examTime);
+    public void Update(Language language, int maxStudents, DateOnly examDate, int teacherId, TimeOnly examTime);
+    public void Delete(int id);
+}
