dbc2c0abb8e4b9c03610ab31f0d5d8ec3ba9cbc0
DamjanVincic <damjanvincic2003@gmail.com>
2024-04-10 22:09:46 +0200

    Add User service

---
+++ LangLang/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using LangLang.Model;
+
+namespace LangLang.Services;
+
+public interface IUserService
+{
+    List<User> GetAll();
+    User? GetById(int id);
+    public void Add(string firstName, string lastName, string email, string password, Gender gender, string phone, Education? education = null, List<Language>? languages = null);
+    public void Update(int id, string firstName, string lastName, string password, Gender gender, string phone, Education? education = null, List<Language>? languages = null);
+    void Delete(int id);
+    User? Login(string email, string password);
+}
---
+++ LangLang/Services/UserService.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LangLang.Model;
+using LangLang.Repositories;
+
+namespace LangLang.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository = new UserFileRepository();
+    
+    public List<User> GetAll()
+    {
+        return _userRepository.GetAll();
+    }
+
+    public User? GetById(int id)
+    {
+        return _userRepository.GetById(id);
+    }
+    
+    public void Add(string firstName, string lastName, string email, string password, Gender gender, string phone, Education? education = null, List<Language>? languages = null)
+    {
+        if (_userRepository.GetAll().Any(user => user.Email.Equals(email)))
+            throw new InvalidInputException("Email already exists");
+
+        if (education is not null)
+            _userRepository.Add(new Student(firstName, lastName, email, password, gender, phone, education));
+        else if (languages is not null)
+            _userRepository.Add(new Teacher(firstName, lastName, email, password, gender, phone, languages));
+        else
+            throw new InvalidInputException("Invalid input");
+        
+    }
+    
+    public void Update(int id, string firstName, string lastName, string password, Gender gender, string phone, Education? education = null, List<Language>? languages = null)
+    {
+        //TODO: Validate if user(student) hasn't applied to any courses or exams
+        User user = _userRepository.GetById(id) ?? throw new InvalidInputException("User doesn't exist");
+        
+        user.FirstName = firstName;
+        user.LastName = lastName;
+        user.Password = password;
+        user.Gender = gender;
+        user.Phone = phone;
+        
+        switch (user)
+        {
+            case Student student:
+                student.Education = education;
+                break;
+            case Teacher teacher:
+                teacher.Qualifications = languages ?? new List<Language>();
+                break;
+        }
+        
+        _userRepository.Update(user);
+    }
+    
+    public void Delete(int id)
+    {
+        _userRepository.Delete(id);
+    }
+    
+    public User? Login(string email, string password)
+    {
+        return _userRepository.GetAll().FirstOrDefault(user => user.Email.Equals(email) && user.Password.Equals(password));
+    }
+}
