874e498d7212796c10aa2937702dbe3bf08b3566
DamjanVincic <damjanvincic2003@gmail.com>
2024-04-13 19:32:22 +0200

    Add Schedule repository

---
+++ LangLang/Repositories/IScheduleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using LangLang.Model;
+
+namespace LangLang.Repositories;
+
+public interface IScheduleRepository
+{
+    public List<ScheduleItem> GetByDate(DateOnly date);
+    public void Add(ScheduleItem item);
+}
---
+++ LangLang/Repositories/ScheduleFileRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using LangLang.Model;
+using Newtonsoft.Json;
+
+namespace LangLang.Repositories;
+
+public class ScheduleFileRepository : IScheduleRepository
+{
+    private const string ScheduleFileName = "schedule.json";
+    private const string ScheduleDirectoryName = "data";
+    
+    private Dictionary<DateOnly, List<ScheduleItem>> _table = new();
+
+    public List<ScheduleItem> GetByDate(DateOnly date)
+    {
+        LoadData();
+        _table.TryGetValue(date, out var items);
+        return items ?? new List<ScheduleItem>();
+    }
+
+    public void Add(ScheduleItem item)
+    {
+        LoadData();
+        
+        if (!_table.ContainsKey(item.Date))
+            _table.Add(item.Date, new List<ScheduleItem>());
+        
+        _table[item.Date].Add(item);
+        
+        SaveData();
+    }
+    
+    private void SaveData()
+    {
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), ScheduleDirectoryName, ScheduleFileName);
+        
+        string json = JsonConvert.SerializeObject(_table, new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            TypeNameHandling = TypeNameHandling.Auto
+        });
+        
+        File.WriteAllText(filePath, json);
+    }
+
+    private void LoadData()
+    {
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), ScheduleDirectoryName, ScheduleFileName);
+        
+        if (!File.Exists(filePath))
+            return;
+        
+        string json = File.ReadAllText(filePath);
+
+        _table = JsonConvert.DeserializeObject<Dictionary<DateOnly, List<ScheduleItem>>>(json, new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Auto
+        }) ?? new Dictionary<DateOnly, List<ScheduleItem>>();
+    }
+}
