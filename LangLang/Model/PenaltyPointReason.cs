163858207554a49e9d8a4535b888690c85f9a129
milicaradicc <milica.t.radic@gmail.com>
2024-04-27 12:33:11 +0200

    Add penalty point class

---
+++ LangLang/Model/PenaltyPointReason.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Model
+{
+    public enum PenaltyPointReason
+    {
+        UCENIK_SE_NIJE_POJAVIO_U_TERMINU,
+        UCENIK_UZNEMIRAVA_OSTALE,
+        UCENIK_NIJE_URADIO_DOMACI,
+        OBRZALOZENJE_ODUSTAJANJA_ODBIJENO
+    }
+}
---
+++ LangLang/Model/PenaltyPoints.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows;
+
+namespace LangLang.Model
+{
+    public class PenaltyPoint
+    {
+        private PenaltyPointReason _penaltyPointReason;
+        private bool _deleted;
+        private int _studentId;
+        private int _courseId;
+        private DateOnly _datePenaltyPointGiven;
+
+        public PenaltyPoint(PenaltyPointReason _penaltyPointReason, bool _deleted, int _studentId, int _courseId, DateOnly _datePenaltyPointGiven)
+        {
+            PenaltyPointReason = _penaltyPointReason;   
+            Deleted = _deleted;
+            StudentId = _studentId;
+            CourseId = _courseId;
+            DatePenaltyPointGiven = _datePenaltyPointGiven;
+        }
+
+        public PenaltyPointReason PenaltyPointReason
+        {
+            get => _penaltyPointReason;
+            set
+            {
+                _penaltyPointReason = value;
+            }
+        }
+
+        public bool Deleted
+        {
+            get => _deleted;
+            set
+            {
+                _deleted = value;
+            }
+        }
+
+        public int StudentId
+        {
+            get => _studentId;
+            set
+            {
+                _studentId = value;
+            }
+        }
+
+        public int CourseId
+        {
+            get => _courseId;
+            set
+            {
+                _courseId = value;
+            }
+        }
+
+        public DateOnly DatePenaltyPointGiven
+        {
+            get => _datePenaltyPointGiven;
+            set
+            {
+                _datePenaltyPointGiven = value;
+            }
+        }
+    }
+}
