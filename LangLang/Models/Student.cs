8db1f34b130e8bb3b16b4a940be58afa894c1828
Damjan Vincic <damjanvincic2003@gmail.com>
2024-04-29 19:04:16 +0200

    Update student model from another branch

--- LangLang/Models/Student.cs
+++ LangLang/Models/Student.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LangLang.Models;
 
 public class Student : User
 {
-    public Education? Education { get; set; }
 
     public Student(string firstName, string lastName, string email, string password, Gender gender, string phone, Education? education)
         : base(firstName, lastName, email, password, gender, phone)
     {
         Education = education ?? throw new ArgumentNullException(nameof(education));
     }
+    public Education? Education { get; set; }
+    public Course? ActiveCourse { get; set; }
+    
+    // obradjeniJezici / zavrseniJezici
+    // dict jezik-bool, kada se zavrsi dodaj sa false, kada polozi ispit promeni na true
+    public Dictionary<int, bool> CoursePassFail { get; set; } = new();
+    public List<int> AppliedCourses { get; set; } = new();
 }
