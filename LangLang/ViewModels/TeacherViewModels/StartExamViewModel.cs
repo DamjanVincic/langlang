a1c19db53043ccadbe3489bf76b947388c822767
Mijat Krivokapić <mijat2004@gmail.com>
2024-04-30 18:02:31 +0200

    Add StartExam view and viewmodel

---
+++ LangLang/ViewModels/TeacherViewModels/StartExamViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using LangLang.Models;
+using LangLang.ViewModels.StudentViewModels;
+using LangLang.Services;
+
+namespace LangLang.ViewModels.TeacherViewModels
+{
+    public class StartExamViewModel:ViewModelBase
+    {
+        private readonly IExamService _examService = new ExamService();
+        public StartExamViewModel(int examId)
+        {
+            Students = new ObservableCollection<SingleStudentViewModel>(_examService.GetStudents(examId)
+                .Select(student => new SingleStudentViewModel(student)));
+        }
+
+        public ObservableCollection<SingleStudentViewModel> Students;
+    }
+}
