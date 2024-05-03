3497fbc00837640f201965262037ea9ed8c14107
Mijat Krivokapić <mijat2004@gmail.com>
2024-05-03 17:56:30 +0200

    Implement ConfirmCommand

--- LangLang/ViewModels/TeacherViewModels/StartExamViewModel.cs
+++ LangLang/ViewModels/TeacherViewModels/StartExamViewModel.cs
@@ -4,22 +4,34 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using LangLang.Models;
 using LangLang.ViewModels.StudentViewModels;
 using LangLang.Services;
+using System.Windows;
+using GalaSoft.MvvmLight.Command;
 
 namespace LangLang.ViewModels.TeacherViewModels
 {
     public class StartExamViewModel:ViewModelBase
     {
+        private readonly int _examId;
         private readonly IExamService _examService = new ExamService();
         public StartExamViewModel(int examId)
         {
-            Students = new ObservableCollection<SingleStudentViewModel>(_examService.GetStudents(examId)
+            _examId = examId;
+            Students = new ObservableCollection<SingleStudentViewModel>(_examService.GetStudents(_examId)
                 .Select(student => new SingleStudentViewModel(student)));
+            ConfirmCommand = new RelayCommand(Confirm);
         }
 
         public ObservableCollection<SingleStudentViewModel> Students { get; set; }
+        public ICommand ConfirmCommand { get; set; }
+
+        public void Confirm()
+        {
+            _examService.ConfirmExam(_examId);
+        }
     }
 }
