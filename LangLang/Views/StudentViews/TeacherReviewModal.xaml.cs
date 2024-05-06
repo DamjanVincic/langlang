4e4711e9c690a5e42f7c61af260d51cabbc97c99
DamjanVincic <damjanvincic2003@gmail.com>
2024-05-06 21:55:41 +0200

    Add modal for inputting teacher review

---
+++ LangLang/Views/StudentViews/TeacherReviewModal.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LangLang.Views.StudentViews;
+
+public partial class TeacherReviewModal : Window
+{
+    public TeacherReviewModal()
+    {
+        InitializeComponent();
+    }
+    
+    public int Response => int.Parse(ResponseTextBox.Text);
+    
+    private void ResponseTextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (int.TryParse(ResponseTextBox.Text, out int number))
+        {
+            if (number is >= 1 and <= 10) return;
+            MessageBox.Show("Please enter a number between 1 and 10.");
+            ResponseTextBox.Text = "";
+        }
+        else
+        {
+            MessageBox.Show("Please enter a valid number.");
+            ResponseTextBox.Text = "";
+        }
+    }
+
+    private void OKButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(ResponseTextBox.Text))
+        {
+            DialogResult = true;
+        }
+        else
+        {
+            MessageBox.Show("Please enter a number between 1 and 10.");
+        }
+    }
+}
