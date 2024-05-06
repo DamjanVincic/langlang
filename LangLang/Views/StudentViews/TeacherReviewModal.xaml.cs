8c98510af6d4acff844b95f48ba9faf07365bede
DamjanVincic <damjanvincic2003@gmail.com>
2024-05-07 00:35:12 +0200

    Fix error message box showing multiple times

--- LangLang/Views/StudentViews/TeacherReviewModal.xaml.cs
+++ LangLang/Views/StudentViews/TeacherReviewModal.xaml.cs
@@ -11,12 +11,16 @@ public partial class TeacherReviewModal : Window
     }
     
     public int Response => int.Parse(ResponseTextBox.Text);
-    
-    private void ResponseTextBox_TextChanged(object sender, TextChangedEventArgs e)
+
+    private void OKButton_Click(object sender, RoutedEventArgs e)
     {
         if (int.TryParse(ResponseTextBox.Text, out int number))
         {
-            if (number is >= 1 and <= 10) return;
+            if (number is >= 1 and <= 10)
+            {
+                DialogResult = true;
+                return;
+            }
             MessageBox.Show("Please enter a number between 1 and 10.");
             ResponseTextBox.Text = "";
         }
@@ -24,18 +28,6 @@ public partial class TeacherReviewModal : Window
         {
             MessageBox.Show("Please enter a valid number.");
             ResponseTextBox.Text = "";
-        }
-    }
-
-    private void OKButton_Click(object sender, RoutedEventArgs e)
-    {
-        if (!string.IsNullOrEmpty(ResponseTextBox.Text))
-        {
-            DialogResult = true;
-        }
-        else
-        {
-            MessageBox.Show("Please enter a number between 1 and 10.");
         }
     }
 }
