8997b5e80066c6db3fa24eb17bd978ca7ec20540
Mijat Krivokapić <mijat2004@gmail.com>
2024-05-24 13:34:54 +0200

    Implement course and exam count

--- LangLang/Services/DirectorService.cs
+++ LangLang/Services/DirectorService.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using LangLang.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LangLang.Models;
 
 namespace LangLang.Services
 {
-    internal class DirectorService
+    internal class DirectorServices:IDirectorService
     {
+        private readonly ICourseRepository _courseRepository = new CourseFileRepository();
+        private readonly IExamRepository _examRepository = new ExamFileRepository();
+
+        public void GenerateLanguageReport()
+        {
+            // LanguageId, Course count
+            Dictionary<int, int> courseCount = new();
+
+            foreach (Course course in _courseRepository.GetAll())
+            {
+                if ((DateTime.Now - course.StartDate.ToDateTime(TimeOnly.MinValue)).TotalDays > 365) continue;
+
+                if (!courseCount.TryAdd(course.Language.Id, 1))
+                    courseCount[course.Language.Id] += 1;
+            }
+
+            // LanguageId, Exam count
+            Dictionary<int, int> examCount = new();
+
+            foreach (Exam exam in _examRepository.GetAll())
+            {
+                if ((DateTime.Now - exam.Date.ToDateTime(TimeOnly.MinValue)).TotalDays > 365) continue;
+
+                if (!examCount.TryAdd(exam.Language.Id, 1))
+                    courseCount[exam.Language.Id] += 1;
+            }
+        }
     }
 }
