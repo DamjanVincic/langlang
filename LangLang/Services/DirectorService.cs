d1e01d2360388710dd785fb45c9e424c266a8aa5
Mijat Krivokapić <mijat2004@gmail.com>
2024-05-26 19:53:28 +0200

    Implement methods for handling pdf

--- LangLang/Services/DirectorService.cs
+++ LangLang/Services/DirectorService.cs
@@ -1,17 +1,31 @@
 ﻿using LangLang.Repositories;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media.Effects;
 using LangLang.Models;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+using OxyPlot;
+using OxyPlot.Legends;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing;
 
 namespace LangLang.Services
 {
-    internal class DirectorServices:IDirectorService
+    public class DirectorService:IDirectorService
     {
         private readonly ICourseRepository _courseRepository = new CourseFileRepository();
         private readonly IExamRepository _examRepository = new ExamFileRepository();
+        private readonly ILanguageRepository _languageRepository = new LanguageFileRepository();
+
+        private const string ReportsFolderName = "Reports";
+        private const string LanguageReportSubfolder = "LanguageReports";
 
         public void GenerateLanguageReport()
         {
@@ -35,6 +49,35 @@ namespace LangLang.Services
 
                 if (!examCount.TryAdd(exam.Language.Id, 1))
                     courseCount[exam.Language.Id] += 1;
+            }
+        }
+
+        private static void SaveToPdf(PlotModel plotModel, string filePath)
+        {
+            using (var stream = File.Create(filePath))
+            {
+                var pdfExporter = new PdfExporter { Width = 600, Height = 400 };
+                pdfExporter.Export(plotModel, stream);
+            }
+        }
+        private static void MergePdf(string outputFilePath, string[] inputFilePaths)
+        {
+            PdfDocument outputPDFDocument = new PdfDocument();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            foreach (string filePath in inputFilePaths)
+            {
+                PdfDocument inputPDFDocument = PdfReader.Open(filePath, PdfDocumentOpenMode.Import);
+                outputPDFDocument.Version = inputPDFDocument.Version;
+                foreach (PdfPage page in inputPDFDocument.Pages)
+                {
+                    outputPDFDocument.AddPage(page);
+                }
+            }
+            outputPDFDocument.Save(outputFilePath);
+
+            foreach (string filePath in inputFilePaths)
+            {
+                File.Delete(filePath);
             }
         }
     }
