aa415643d42fd0907843c66aa557a2a545ea71c2
DamjanVincic <damjanvincic2003@gmail.com>
2024-05-26 21:26:30 +0200

    Register dependencies for injection

--- LangLang/App.xaml.cs
+++ LangLang/App.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows;
 using LangLang.Models;
 using LangLang.Repositories;
+using LangLang.Services;
+using LangLang.ViewModels.CourseViewModels;
+using LangLang.ViewModels.DirectorViewModels;
+using LangLang.ViewModels.ExamViewModels;
+using LangLang.ViewModels.StudentViewModels;
+using LangLang.ViewModels.TeacherViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using ServiceProvider = LangLang.Models.ServiceProvider;
 
@@ -30,8 +35,40 @@ namespace LangLang
         
         private void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ICourseGradeRepository, CourseGradeFileRepository>();
+            services.AddScoped<ICourseRepository, CourseFileRepository>();
+            services.AddScoped<IExamGradeRepository, ExamGradeFileRepository>();
+            services.AddScoped<IExamRepository, ExamFileRepository>();
+            services.AddScoped<ILanguageRepository, LanguageFileRepository>();
+            services.AddScoped<IMessageRepository, MessageFileRepository>();
+            services.AddScoped<IPenaltyPointRepository, PenaltyPointFileRepository>();
+            services.AddScoped<IScheduleRepository, ScheduleFileRepository>();
+            services.AddScoped<IUserRepository, UserFileRepository>();
+
+            services.AddScoped<ICourseGradeService, CourseGradeService>();
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<IDirectorService, DirectorService>();
+            services.AddScoped<IExamGradeService, ExamGradeService>();
+            services.AddScoped<IExamService, ExamService>();
+            services.AddScoped<ILanguageService, LanguageService>();
+            services.AddScoped<IMessageService, MessageService>();
+            services.AddScoped<IPenaltyPointService, PenaltyPointService>();
+            services.AddScoped<IScheduleService, ScheduleService>();
+            services.AddScoped<IStudentService, StudentService>();
+            services.AddScoped<ITeacherService, TeacherService>();
+            services.AddScoped<IUserService, UserService>();
+
             services.AddTransient<MainWindow>();
-            services.AddSingleton<IUserRepository, UserFileRepository>();
+            services.AddTransient<ActiveCoursesViewModel>();
+            services.AddTransient<AddCourseViewModel>();
+            services.AddTransient<CourseListingViewModel>();
+            services.AddTransient<CoursesWithStudentWithdrawalsViewModel>();
+            services.AddTransient<StartableCoursesViewModel>();
+            services.AddTransient<GradedExamsViewModel>();
+            services.AddTransient<ExamListingViewModel>();
+            services.AddTransient<AppliedExamListingViewModel>();
+            services.AddTransient<StudentExamViewModel>();
+            services.AddTransient<StartableExamsViewModel>();
         }
         
         protected override void OnStartup(StartupEventArgs e)
