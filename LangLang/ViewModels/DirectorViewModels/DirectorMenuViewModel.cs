5fd00ab359ea8362600fb7b4a4baf72a86997079
zoricnadja <zoric.n2003@gmail.com>
2024-05-28 00:07:25 +0200

    Update director main menu

--- LangLang/ViewModels/DirectorViewModels/DirectorMenuViewModel.cs
+++ LangLang/ViewModels/DirectorViewModels/DirectorMenuViewModel.cs
@@ -5,6 +5,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using LangLang.Models;
 using LangLang.Services;
+using LangLang.Services.ReportServices;
 using LangLang.Views.DirectorViews;
 using LangLang.Views.TeacherViews;
 
@@ -15,6 +16,7 @@ namespace LangLang.ViewModels.DirectorViewModels
         private readonly Director _director = UserService.LoggedInUser as Director ?? throw new InvalidInputException("No one is logged in.");
         private readonly Window _directorViewWindow;
         private readonly IUserService _userService = new UserService();
+        private readonly IGradeReportService _gradeReportService = new GradeReportService();
 
         public DirectorMenuViewModel(Window directorViewWindow)
         {
@@ -22,11 +24,19 @@ namespace LangLang.ViewModels.DirectorViewModels
             ViewTeachersCommand = new RelayCommand(ViewTeachers);
             SendOutGradesCommand = new RelayCommand(SendOutGrades);
             LogOutCommand = new RelayCommand(LogOut);
+            PenaltyPointReportCommand = new RelayCommand(GeneratePenaltyPointReport);
+            GradeReportCommand = new RelayCommand(GenerateGradeReport);
+            PointReportCommand = new RelayCommand(GeneratePointReport);
+            LanguageReportCommand = new RelayCommand(GenerateLanguageReport);
         }
 
         public RelayCommand ViewTeachersCommand { get; }
         public RelayCommand SendOutGradesCommand { get; }
+        public ICommand PenaltyPointReportCommand { get; }
         public ICommand LogOutCommand { get; }
+        public ICommand GradeReportCommand { get; }
+        public ICommand PointReportCommand { get; }
+        public ICommand LanguageReportCommand { get; }
 
         private void LogOut()
         {
@@ -44,6 +54,22 @@ namespace LangLang.ViewModels.DirectorViewModels
         {
             var sendGradesView = new GradedExams();
             sendGradesView.Show();
+        }
+        private void GeneratePenaltyPointReport()
+        {
+            throw new NotImplementedException();
+        }
+        private void GenerateGradeReport()
+        {
+            _gradeReportService.GenerateGradeReport();
+        }
+        private void GeneratePointReport()
+        {
+            throw new NotImplementedException();
+        }
+        private void GenerateLanguageReport()
+        {
+            throw new NotImplementedException();
         }
     }
 }
