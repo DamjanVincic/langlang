58d2a853761f3cd3e3bd4a201423ceff9b1cce44
milicaradicc <milica.t.radic@gmail.com>
2024-05-27 10:50:03 +0200

    Remove old report

--- LangLang/ViewModels/DirectorViewModels/DirectorMenuViewModel.cs
+++ LangLang/ViewModels/DirectorViewModels/DirectorMenuViewModel.cs
@@ -54,56 +54,7 @@ namespace LangLang.ViewModels.DirectorViewModels
         }
         private void ScoreReport()
         {
-            // ensure that the directories exist
-            string reportsFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "data", "reports", "scores and pass rate");
-            Directory.CreateDirectory(reportsFolderPath);
-
-            // construct the file path
-            string fileName = DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".pdf";
-            string filePath = Path.Combine(reportsFolderPath, fileName);
-
-            // create the PDF document
-            using (Document document = new Document())
-            {
-                PdfWriter.GetInstance(document, new FileStream(filePath, FileMode.Create));
-                document.Open();
-
-                Paragraph paragraph = new Paragraph("The average score achieved on each part of all exams in the last year");
-                paragraph.Alignment = Element.ALIGN_CENTER; 
-                document.Add(paragraph);
-
-                // new line so it is not so close together
-                document.Add(Chunk.NEWLINE);
-
-
-                // set number od cols
-                Dictionary<int, List<int>> data = _examService.AveragePointsInLastYear();
-                PdfPTable table = new PdfPTable(5);   // exam id and all the points
-
-                // headers
-                table.AddCell("Exam");
-                table.AddCell("Listening");
-                table.AddCell("Talking");
-                table.AddCell("Writing");
-                table.AddCell("Reading");
-
-                foreach(KeyValuePair<int,List<int>> pair in data)
-                {
-                    table.AddCell(pair.Key.ToString());
-                    foreach(int points in pair.Value)
-                    {
-                        if (points > 0)
-                            table.AddCell(points.ToString());
-                        else
-                            table.AddCell("not graded");
-                    }
-                }
-
-                document.Add(table);
-
-                document.Close();
-
-                MessageBox.Show("Report created!.", "Success", MessageBoxButton.OK,MessageBoxImage.Information);
+            
             }
         }
     }
