570c4959ec5ff22aa65f3bab522386a7848e1972
milicaradicc <milica.t.radic@gmail.com>
2024-05-28 19:55:01 +0200

    Update reports

--- LangLang/Services/ReportServices/PassRateReportService.cs
+++ LangLang/Services/ReportServices/PassRateReportService.cs
@@ -7,6 +7,10 @@ using LangLang.Models;
 using OxyPlot.Series;
 using OxyPlot.Axes;
 using OxyPlot;
+using System.Windows.Markup;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf;
+using System.Text;
 
 namespace LangLang.Services.ReportServices
 {
@@ -15,24 +19,49 @@ namespace LangLang.Services.ReportServices
 
         private readonly IExamRepository _examRepository;
         private readonly IExamGradeService _examGradeService;
+        private readonly ICourseService _courseService;
+        private readonly ICourseGradeService _courseGradeService;
         private const string ReportsFolderName = "Reports";
         private const string PassRateReportSubfolder = "PassRateReports";
-        public PassRateReportService(IExamRepository examRepository, IExamGradeService examGradeService) {
+        public PassRateReportService(IExamRepository examRepository, IExamGradeService examGradeService,ICourseService courseService, ICourseGradeService courseGradeService) {
             _examRepository = examRepository;
             _examGradeService = examGradeService;
+            _courseService = courseService;
+            _courseGradeService = courseGradeService;
         }
         public void GeneratePointsPassRateReport()
         {
             Directory.CreateDirectory(Path.Combine(ReportsFolderName, PassRateReportSubfolder));
 
             List<double> pointsAvg = AveragePointsInLastYear();
-
             PlotModel gradeAvgPlotModel = CreatePointsAvgPlotModel(pointsAvg);
 
-            string pointsAvgPath = Path.Combine(ReportsFolderName, PassRateReportSubfolder, DateTime.Now.ToString("yyyy-MMMM-dd-hh-mm") + ".pdf");
+            string pointsAvgPath = Path.Combine(ReportsFolderName, PassRateReportSubfolder, "avg" + ".pdf");
             SaveToPdf(gradeAvgPlotModel, pointsAvgPath);
 
-            EmailService.SendMessage("PassRate report", "Today's PassRate report is attached in this email", pointsAvgPath);
+            Dictionary<int, double> attended = GetAttendedCourseAverage();
+            PlotModel attendedeAvgPlotModel = CreatePlotModel("Number of students who have attended courses",attended);
+
+            string attendedAvgPath = Path.Combine(ReportsFolderName, PassRateReportSubfolder, "attended" + ".pdf");
+            SaveToPdf(attendedeAvgPlotModel, attendedAvgPath);
+
+            Dictionary<int, double> passed = GetPassedCourseAverage();
+            PlotModel passedAvgPlotModel = CreatePlotModel("Number of students who have passed courses",passed);
+
+            string passedAvgPath = Path.Combine(ReportsFolderName, PassRateReportSubfolder, "passed" + ".pdf");
+            SaveToPdf(passedAvgPlotModel, passedAvgPath);
+
+            Dictionary<int, double> percentage = GetPercentagePassed(attended, passed);
+            PlotModel percentageAvgPlotModel = CreatePlotModel("Percentage of students who have passed courses", percentage);
+
+            string percentageAvgPath = Path.Combine(ReportsFolderName, PassRateReportSubfolder, "percentage" + ".pdf");
+            SaveToPdf(percentageAvgPlotModel, percentageAvgPath);
+
+            string reportPath = Path.Combine(ReportsFolderName, PassRateReportSubfolder,
+                DateTime.Now.ToString("yyyy-MMMM-dd-hh-mm") + ".pdf");
+
+            MergePdf(reportPath, new[] { pointsAvgPath, attendedAvgPath, passedAvgPath, percentageAvgPath });
+            EmailService.SendMessage("PassRate report", "Today's PassRate report is attached in this email", reportPath);
         }
         private static PlotModel CreatePointsAvgPlotModel(List<double> data)
         {
@@ -48,7 +77,7 @@ namespace LangLang.Services.ReportServices
             var xAxis = new CategoryAxis
             {
                 Position = AxisPosition.Bottom,
-                Title = "Grades"
+                Title = "Points"
             };
             xAxis.Labels.Add("ListeningPoints");
             xAxis.Labels.Add("TalkingPoints");
@@ -60,6 +89,63 @@ namespace LangLang.Services.ReportServices
 
             return model;
         }
+        private PlotModel CreatePlotModel(string title, Dictionary<int, double> data)
+        {
+            var model = new PlotModel { Title = title };
+
+            var categoryAxis = new CategoryAxis { Position = AxisPosition.Left };
+            categoryAxis.Labels.AddRange(data.Keys.Select(id => id.ToString() + " " + _courseService.GetById(id).Language!.ToString() + " " + _courseService.GetById(id).Date!.ToString()).ToList());
+
+            var valueAxis = new LinearAxis { Position = AxisPosition.Bottom, MinimumPadding = 0, MaximumPadding = 0.06, AbsoluteMinimum = 0 };
+
+            var barSeries = new BarSeries { StrokeColor = OxyColors.Black, StrokeThickness = 1, FillColor = OxyColors.Red }; // Postavljanje boje na crvenu
+            barSeries.ActualItems.AddRange(data.Values.Select(value => new BarItem { Value = value }).ToList());
+
+            model.Series.Add(barSeries);
+            model.Axes.Add(categoryAxis);
+            model.Axes.Add(valueAxis);
+
+            return model;
+        }
+
+        private Dictionary<int, double> GetAttendedCourseAverage()
+        {
+            Dictionary<int,double> result = new Dictionary<int,double>();
+            foreach(Course course in _courseService.GetAll())
+            {
+                if (course.IsFinished)
+                    result[course.Id] = _courseGradeService.GetByCourseId(course.Id).Count();
+            }
+            return result;
+        }
+        private Dictionary<int, double> GetPassedCourseAverage()
+        {
+            Dictionary<int, double> result = new Dictionary<int, double>();
+            foreach (Course course in _courseService.GetAll())
+            {
+                if (course.IsFinished)
+                {
+                    var courseGrades = _courseGradeService.GetByCourseId(course.Id);
+                    var passedCount = courseGrades.Count(courseGrade => courseGrade.KnowledgeGrade > 5 && courseGrade.ActivityGrade > 5);
+                    result[course.Id] = passedCount;
+                }
+            }
+            return result;
+        }
+
+        private Dictionary<int, double> GetPercentagePassed(Dictionary<int, double> listened, Dictionary<int, double> passed)
+        {
+            Dictionary<int, double> result = new Dictionary<int, double>();
+            foreach (int courseId in listened.Keys)
+            {
+                if (listened[courseId] != 0)
+                    result[courseId] = (passed[courseId] / listened[courseId]) * 100; // Deljenje decimalnim brojem
+                else
+                    result[courseId] = 0;
+            }
+            return result;
+        }
+
         private static void SaveToPdf(PlotModel plotModel, string filePath)
         {
             using var stream = File.Create(filePath);
@@ -103,6 +189,26 @@ namespace LangLang.Services.ReportServices
             }
 
             return sumOfPoints;
+        }
+        private static void MergePdf(string outputFilePath, string[] inputFilePaths)
+        {
+            PdfDocument outputPdfDocument = new PdfDocument();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            foreach (string filePath in inputFilePaths)
+            {
+                PdfDocument inputPdfDocument = PdfReader.Open(filePath, PdfDocumentOpenMode.Import);
+                outputPdfDocument.Version = inputPdfDocument.Version;
+                foreach (PdfPage page in inputPdfDocument.Pages)
+                {
+                    outputPdfDocument.AddPage(page);
+                }
+            }
+            outputPdfDocument.Save(outputFilePath);
+
+            foreach (string filePath in inputFilePaths)
+            {
+                File.Delete(filePath);
+            }
         }
     }
 }
