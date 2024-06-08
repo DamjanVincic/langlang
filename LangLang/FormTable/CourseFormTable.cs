c78f9d8bb0c43e94ff50f6fe449ed5ad2613da6f
milicaradicc <milica.t.radic@gmail.com>
2024-06-09 00:42:32 +0200

    Add FormTable and basic course menu

---
+++ LangLang/FormTable/CourseFormTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using LangLang.Models;
+namespace LangLang.FormTable;
+public class CourseFormTable
+{
+    Type type = typeof(Course);
+    
+
+    public void CourseMenu()
+    {
+        while (true)
+        {
+            Console.WriteLine("Chose option for course \n" +
+                "1) create \n" +
+                "2) update \n" +
+                "3) delete \n" +
+                "4) list all exams \n" +
+                "q)uit");
+            string userInput = Console.ReadLine();
+            if (userInput == "1") 
+                Add();
+            if (userInput == "2")
+                Update();
+            if (userInput == "3")
+                Delete();
+            if(userInput == "4")
+                Read();
+            if (userInput == "q" || userInput == "Q")
+                break;
+            else
+                Console.WriteLine("invalid option \n");
+        }
+    }
+    public void Add() { }
+    public void Update() { }
+    public void Delete() { }
+    public void Read() { }
+}
---
+++ LangLang/FormTable/FormTableMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.FormTable
+{
+    public class FormTableMenu
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
