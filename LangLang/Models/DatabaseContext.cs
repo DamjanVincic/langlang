3caf77b897c9e3d24535ad7fa5c2cbfc6c0b97d5
DamjanVincic <damjanvincic2003@gmail.com>
2024-06-11 14:39:10 +0200

    Fix not being able to make migrations

--- LangLang/Models/DatabaseContext.cs
+++ LangLang/Models/DatabaseContext.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace LangLang.Models;
 
 public class DatabaseContext : DbContext
 {
+    public DatabaseContext()
+    {
+    }
+
     public DatabaseContext(DbContextOptions options) : base(options)
     {
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            DotNetEnv.Env.Load("../.env"); // Works if the current directory is the LangLang project
+            string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? throw new InvalidInputException("Connection string not found in .env file.");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
     }
 
     public DbSet<Course> Courses { get; set; }
@@ -22,5 +39,12 @@ public class DatabaseContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Course>()
+            .Property(c => c.Held)
+            .HasConversion(
+                v => JsonConvert.SerializeObject(v), // Convert list to JSON string
+                v => JsonConvert.DeserializeObject<List<Weekday>>(v) // Convert JSON string to list
+            );
     }
 }
