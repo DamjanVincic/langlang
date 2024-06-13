e85e6953f9e45d64d7861859f9b0a1306341ada9
Mijat Krivokapić <mijat2004@gmail.com>
2024-06-13 12:18:16 +0200

    Add user migrations

---
+++ LangLang/Migrations/20240613100600_AddUserTable.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LangLang.Migrations
+{
+    public partial class AddUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeacherId",
+                table: "Languages",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Dictionary<int, bool>",
+                columns: table => new
+                {
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dictionary<int, int>",
+                columns: table => new
+                {
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Gender = table.Column<int>(type: "integer", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Discriminator = table.Column<string>(type: "text", nullable: false),
+                    Education = table.Column<int>(type: "integer", nullable: true),
+                    PenaltyPoints = table.Column<int>(type: "integer", nullable: true),
+                    ActiveCourseId = table.Column<int>(type: "integer", nullable: true),
+                    LanguagePassFail = table.Column<string>(type: "text", nullable: true),
+                    AppliedExams = table.Column<List<int>>(type: "integer[]", nullable: true),
+                    ExamGradeIds = table.Column<string>(type: "text", nullable: true),
+                    CourseGradeIds = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_TeacherId",
+                table: "Languages",
+                column: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Languages_Users_TeacherId",
+                table: "Languages",
+                column: "TeacherId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Languages_Users_TeacherId",
+                table: "Languages");
+
+            migrationBuilder.DropTable(
+                name: "Dictionary<int, bool>");
+
+            migrationBuilder.DropTable(
+                name: "Dictionary<int, int>");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Languages_TeacherId",
+                table: "Languages");
+
+            migrationBuilder.DropColumn(
+                name: "TeacherId",
+                table: "Languages");
+        }
+    }
+}
