ac646b143ba803f1bfec2c324f492169074da1e2
DamjanVincic <damjanvincic2003@gmail.com>
2024-06-14 15:12:29 +0200

    Remove DI file repositories replaced with database

--- LangLang/App.xaml.cs
+++ LangLang/App.xaml.cs
@@ -55,9 +55,7 @@ namespace LangLang
         private void ConfigureServices(IServiceCollection services)
         {
             ConfigureFileRepositories(services);
-            
-            // TODO: Uncomment this line to use the database
-            //ConfigureDatabaseRepositories(services);
+            ConfigureDatabaseRepositories(services);
 
             services.AddScoped<ICourseGradeService, CourseGradeService>();
             services.AddScoped<ICourseService, CourseService>();
@@ -93,14 +91,9 @@ namespace LangLang
         private static void ConfigureFileRepositories(IServiceCollection services)
         {
             services.AddScoped<ICourseGradeRepository, CourseGradeFileRepository>();
-            services.AddScoped<ICourseRepository, CourseFileRepository>();
             services.AddScoped<IExamGradeRepository, ExamGradeFileRepository>();
-            services.AddScoped<IExamRepository, ExamFileRepository>();
-            services.AddScoped<ILanguageRepository, LanguageFileRepository>();
             services.AddScoped<IMessageRepository, MessageFileRepository>();
             services.AddScoped<IPenaltyPointRepository, PenaltyPointFileRepository>();
-            services.AddScoped<IScheduleRepository, ScheduleFileRepository>();
-            services.AddScoped<IUserRepository, UserFileRepository>();
         }
         
         private void ConfigureDatabaseRepositories(IServiceCollection services)
